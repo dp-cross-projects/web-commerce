78c21640531de412d6c660718aa76f5d6cfae533
dp-cross-projects <l.d.parra@outlook.com>
2026-03-11 19:03:34 -0300

    added user data form

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,8 @@
-from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 
-class UserDataForm(forms.Form):
-    first_name = forms.CharField(max_length=200, label='First Name')
-    last_name = forms.CharField(max_length=200, label='Last Name')
-    email = forms.EmailField(label='Email')
+# This class is for User form on user profile
+class UserDataForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
--- user/views.py
+++ user/views.py
@@ -3,35 +3,46 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from .models import UserHistory, UserOrder
 from checkout.models import CheckOut
-# from django.forms import ModelForm
 from .forms import UserDataForm
+
 ## FRONTEND FUNCTIONS
 
 # Render User Profile
 def user_page(request):
 
-    # Get User logged
-    user = request.user
+    if request.method == 'POST':
+        user = request.user
+        form = UserDataForm(request.POST, instance=user)
+        form.save()
 
-    # Get User Orders
-    user_order = UserOrder.objects.filter(user=request.user)
+        return redirect('/user')
+    else:
+        # Get User logged
+        user = request.user
 
-    # Init User purchased items from User History
-    user_history = []
+        # Get User Orders
+        user_order = UserOrder.objects.filter(user=request.user)
 
-    # Iterate and set User purchased items from User History
-    for item in user_order:
-        # Filter items from User Order
-        user_history.append(UserHistory.objects.filter(user_order=item))        
-    
-    # Render User Profile using user data, user orders and
-    # user history filtered by user order, and user data form
-    return render(request, 'user/user_profile.html',{
-        'user': user,
-        'user_order': user_order,
-        'user_history': user_history,
-        'user_data_form': UserDataForm
-    })
+        # Init User purchased items from User History
+        user_history = []
+
+        # Iterate and set User purchased items from User History
+        for item in user_order:
+            # Filter items from User Order
+            user_history.append(UserHistory.objects.filter(user_order=item))
+
+        # Get user data for form
+        user_data = User.objects.get(pk=user.id)
+        form = UserDataForm(instance=user_data)        
+        
+        # Render User Profile using user data, user orders and
+        # user history filtered by user order, and user data form
+        return render(request, 'user/user_profile.html',{
+            'user': user,
+            'user_order': user_order,
+            'user_history': user_history,
+            'user_data_form': form
+        })
 
 # Complete purchase and move from cart to User Order & User History
 def complete_purchase(request):
